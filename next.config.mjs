2d9aaaf71307d69d37595c212aca971a5f6c368d
Ayan Mukherjee <116819600+Ayan1024@users.noreply.github.com>
2025-07-06 20:26:20 +0530

    fix: add redirects for case-insensitive paths

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/Generate',
+        destination: '/generate',
+        permanent: true,
+      },
+      {
+        source: '/Profilepic',
+        destination: '/profilepic',
+        permanent: true,
+      },
+    ]
+  },
+}
 
 export default nextConfig;
